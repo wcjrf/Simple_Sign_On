9a892a50db73d67f6c869cbf2a8e0ad6a2c75105
wcjrf <37749046+wcjrf@users.noreply.github.com>
2018-04-02 21:48:06 -0700

    dsf

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'net/http'
 require 'json'
 require 'SQLite3'
 
-CLIENT_ID = '7e2d747e91a7c32b0f9af8c60c8864f2bc075ed6117f713980584c40c826f46a'
-CLIENT_SECRET= '5537a8755dd49e2ab5cdcb4ec70349080d4b44da0464033f5df619bf122376a7'
-REDIRECT_URL = 'https://lewistest2.herokuapp.com/callback'
+CLIENT_ID='7e2d747e91a7c32b0f9af8c60c8864f2bc075ed6117f713980584c40c826f46a'
+CLIENT_SECRET='5537a8755dd49e2ab5cdcb4ec70349080d4b44da0464033f5df619bf122376a7'
+REDIRECT_URL='https://lewistest2.herokuapp.com/callback'
 
 enable :sessions
 
